0105d52830410d9beb644ee7549a64a10f96edd0
Carlos Ramirez <karlossander92@gmail.com>
2023-05-31 16:50:32 -0500

    Organizar nuestro Context

--- POS/POS.Api/Program.cs
+++ POS/POS.Api/Program.cs
@@ -1,6 +1,9 @@
+using POS.Infrastrucure.Extensions;
 var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddInjectionInfrastructure(Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
