2bbc105977a752725dada34f05c0d3759d56c02d
Carlos Ramirez <karlossander92@gmail.com>
2023-09-28 21:25:10 -0500

    Métodos CRUD en CategoryRepository

--- POS/POS.Infrastructure/Persistences/Repositories/CategoryRepository.cs
+++ POS/POS.Infrastructure/Persistences/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using POS.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
 using POS.Infrastructure.Commons.Bases.Request;
 using POS.Infrastructure.Commons.Bases.Response;
 using POS.Infrastructure.Persistences.Context;
@@ -20,34 +21,99 @@ namespace POS.Infrastructure.Persistences.Repositories
             _context = context;
         }
 
-        public Task<BaseEntityResponse<Category>> ListCategories(BaseFiltersRequest filters)
+        public async Task<BaseEntityResponse<Category>> ListCategories(BaseFiltersRequest filters)
         {
-            throw new NotImplementedException();
+            var response = new BaseEntityResponse<Category>();
+
+            var categories = (from c in _context.Categories
+                              where c.AuditDeleteUser == null && c.AuditDeleteDate == null
+                              select c)
+                              .AsNoTracking()
+                              .AsQueryable();
+
+            if(filters.NumeroFilter is not null && !string.IsNullOrEmpty(filters.TextFilter))
+            {
+                switch(filters.NumeroFilter)
+                {
+                    case 1:
+                        categories = categories.Where(x => x.Name!.Contains(filters.TextFilter)); 
+                        break;
+                    case 2:
+                        categories = categories.Where(x => x.Description!.Contains(filters.TextFilter));
+                        break;
+                }
+            }
+
+            if(filters.StateFilter is not null)
+            {
+                categories = categories.Where(x => x.State.Equals(filters.StateFilter));
+            }
+
+            if(!string.IsNullOrEmpty(filters.StartDate) && !string.IsNullOrEmpty(filters.EndDate))
+            {
+                categories = categories.Where(x => x.AuditCreateDate >= Convert.ToDateTime(filters.StartDate) && x.AuditCreateDate <= Convert.ToDateTime(filters.EndDate)
+                                                .AddDays(1));
+            }
+
+            if (filters.Sort is null) filters.Sort = "CategoryId";
+
+            response.TotalRecords = await categories.CountAsync();
+            response.Items = await Ordering(filters, categories, !(bool)filters.Download!).ToListAsync();
+            return response;
         }
 
-        public Task<IEnumerable<Category>> ListSelectCategories()
+        public async Task<IEnumerable<Category>> ListSelectCategories()
         {
-            throw new NotImplementedException();
+            var categories = await _context.Categories
+                .Where(x => x.State.Equals(1) && x.AuditDeleteUser == null && x.AuditDeleteDate == null)
+                .AsNoTracking()
+                .ToListAsync();
+            return categories;
         }
 
-        public Task<Category> CategoryById(int categoryId)
+        public async Task<Category> CategoryById(int categoryId)
         {
-            throw new NotImplementedException();
+            var category = await _context.Categories.AsNoTracking()
+                                .FirstOrDefaultAsync(x => x.CategoryId == categoryId);
+            return category;
         }
 
-        public Task<bool> RegisterCategory(Category category)
+        public async Task<bool> RegisterCategory(Category category)
         {
-            throw new NotImplementedException();
+            category.AuditCreateUser = 1;
+            category.AuditCreateDate = DateTime.Now;
+
+            await _context.AddAsync(category);
+            
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected > 0;
         }
 
-        public Task<bool> EditCategory(Category category)
+        public async Task<bool> EditCategory(Category category)
         {
-            throw new NotImplementedException();
+            category.AuditUpdateUser = 1;
+            category.AuditUpdateDate = DateTime.Now;
+
+            _context.Update(category);
+            _context.Entry(category).Property(x => x.AuditCreateUser).IsModified = false;
+            _context.Entry(category).Property(x => x.AuditCreateDate).IsModified = false;
+
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected > 0;
         }
 
-        public Task<bool> RemoveCategory(int categoryId)
+        public async Task<bool> RemoveCategory(int categoryId)
         {
-            throw new NotImplementedException();
+            var category = await _context.Categories.AsNoTracking()
+                .SingleOrDefaultAsync(x => x.CategoryId == categoryId);
+
+            category.AuditDeleteUser = 1;
+            category.AuditDeleteDate = DateTime.Now;
+
+            _context.Update(category);
+
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected > 0;
         }
     }
 }
--- POS/POS.Infrastructure/Persistences/Repositories/GenericRepository.cs
+++ POS/POS.Infrastructure/Persistences/Repositories/GenericRepository.cs
@@ -1,14 +1,19 @@
-﻿using POS.Infrastructure.Persistences.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Helpers;
+using POS.Infrastructure.Persistences.Interfaces;
+using System.Linq.Dynamic.Core;
 
 namespace POS.Infrastructure.Persistences.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        protected 
+        protected IQueryable<TDTO> Ordering<TDTO>(BasePaginacionRequest request, IQueryable<TDTO> queryable, bool pagination = false) where TDTO : class
+        {
+            IQueryable<TDTO> queryDto = request.Order == "desc" ? queryable.OrderBy($"{request.Sort} descending") : queryable.OrderBy($"{request.Sort} ascending");
+
+            if (pagination) queryDto = queryDto.Paginate(request);
+
+            return queryDto;
+        }
     }
 }
